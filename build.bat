0771c9e43c92101c98ebf7f6f262f9c1fec1847d
Kryten2k35 <kryten2k35@gmail.com>
2014-07-20 22:06:44 +0100

    BUILD: Added data folder to build

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 ::UltimaROM build script
-tools\7za.exe a -up0q0r2x2y2z1w2 -mx9 -xr@tools/exclusion.txt UltimaROM_4.4.2.zip META-INF system ultima
+tools\7za.exe a -up0q0r2x2y2z1w2 -mx9 -xr@tools/exclusion.txt UltimaROM_4.4.2.zip META-INF system ultima data
 cd overlay
 ..\tools\7za.exe u -mx9 -up1q1r2x2y2z1w2 -xr@..\tools\exclusion.txt ..\UltimaROM_4.4.2.zip system
 cd ..
