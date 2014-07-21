f01c5fc8427f49af81f2f7b82b4c1898f15db027
Kryten2k35 <kryten2k35@gmail.com>
2014-07-21 22:43:06 +0100

    UltimaROM v17.1
    commit f5a028b5e6768af5a7f51afc819bf451e99fcef1
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Mon Jul 21 20:12:17 2014 +0100
    
        BUILD: Now we build using verison number and codename from text files within AROMA
    
    commit 8255c82d4533fee15290d251ea2f9702c9753284
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Mon Jul 21 19:28:18 2014 +0100
    
        AROMA: Verison increase for splash image, adn stored splash image pspimage file for later use
    
    commit eb1e55b029c2b4f974a58e28633a43d87297899c
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Mon Jul 21 13:51:20 2014 +0100
    
        AROMA: Updated changlog
    
    commit 0e0f46b099c389b86e842ee088a41122c6c5d63f
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Mon Jul 21 12:50:56 2014 +0100
    
        APP: Fixed a small derp in the control app
    
    commit dd9f9eb4733cf1284b4f95192c9c7be28bdc1176
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Mon Jul 21 12:50:38 2014 +0100
    
        GIT: Ignoring patch folder I keep in this same folder
    
    commit 52d299b2d4cb6696bcca16bcf9dda1948325ba58
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Mon Jul 21 03:57:35 2014 +0100
    
        APP: Left this in there, somehow. Should have been deleted
    
    commit c9f30374fe29b50432d41db46ebc13859892eecd
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Mon Jul 21 02:34:40 2014 +0100
    
        APP: Removed PageBuddyNotif... Not a popular choice, I'm sure, but it's ugly and I cannot make it bigger.
    
    commit 3ecedcb1cf12f4de6a799ef10e350f9b79022a26
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Sun Jul 20 22:22:35 2014 +0100
    
        BLOAT: Removed Simple Widget. I missed this the first time around
    
    commit 4b097b01fc7f9ce8288579a515763650d7d62bb4
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Sun Jul 20 22:16:01 2014 +0100
    
        AROMA: Fixed install bug when user picked a preset
    
    commit 0771c9e43c92101c98ebf7f6f262f9c1fec1847d
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Sun Jul 20 22:06:44 2014 +0100
    
        BUILD: Added data folder to build
    
    commit a46640addfba07902850e8287c6b53500d815d09
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Sun Jul 20 22:05:35 2014 +0100
    
        AROMA/APP: Added Terminal app
    
    commit 562e8e1e8d4b06637900b62716b7c9e65aff70a9
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Sun Jul 20 20:17:13 2014 +0100
    
        APP: Possible Bluetooth fix, readded PageBuddy settings
    
    commit 2828be352e52c686037cab6d39af098d8229f770
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Sun Jul 20 20:16:14 2014 +0100
    
        LIB: Possible AllShare cast fix
    
    commit 9444cdd8a1d425e4da5b0524a188bccd34cfb4c2
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Sun Jul 20 18:20:12 2014 +0100
    
        LOCKSCREEN: Fixed SIM PIN, fully this time
    
    commit 45bee4f79afd1050a917fa715148cb5cd8a37478
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Sun Jul 20 17:24:34 2014 +0100
    
        APPS: Added conference calling, call recording and auto recording to Samsung Phone and Contacts
    
    commit b52188f5f1085a61917620978beb3c4d6eb539b9
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Sun Jul 20 17:19:10 2014 +0100
    
        APP: Fixed Calendar sync FC's when signing into Google, using Samsung Contacts and Phone
    
    commit 76462abb05c05b6179f144a4c136f2a347a9ca19
    Author: Kryten2k35 <kryten2k35@gmail.com>
    Date:   Sun Jul 20 01:42:30 2014 +0100
    
        Lockscreen: Fixed SIM PIN failing when using AOSP lockscreen

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 ::UltimaROM build script
-tools\7za.exe a -up0q0r2x2y2z1w2 -mx9 -xr@tools/exclusion.txt UltimaROM_4.4.2.zip META-INF system ultima
+set /p CODENAME=<META-INF\com\google\android\aroma\codename.txt
+set /p VERSION=<META-INF\com\google\android\aroma\version.txt
+tools\7za.exe a -up0q0r2x2y2z1w2 -mx9 -xr@tools/exclusion.txt UltimaROM_%VERSION%_%CODENAME%.zip META-INF system ultima data
 cd overlay
-..\tools\7za.exe u -mx9 -up1q1r2x2y2z1w2 -xr@..\tools\exclusion.txt ..\UltimaROM_4.4.2.zip system
+..\tools\7za.exe u -mx9 -up1q1r2x2y2z1w2 -xr@..\tools\exclusion.txt ..\UltimaROM_%VERSION%_%CODENAME%.zip system
 cd ..
--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/system/bin/sh
 # UltimaROM build script
-FILE=UltimaROM_4.4.2.zip
+AROMA=META-INF/com/google/android/aroma
+CODENAME=$(cat ${AROMA}/codename.txt)
+VERSION=$(cat ${AROMA}/version.txt)
+FILE=UltimaROM_${VERSION}_${CODENAME}.zip
 
-tools/7za a -up0q0r2x2y2z1w2 -mx9 -xr@tools/exclusion.txt ${FILE} META-INF system ultima
+tools/7za a -up0q0r2x2y2z1w2 -mx9 -xr@tools/exclusion.txt ${FILE} META-INF system ultima data
 cd overlay
 ../tools/7za u -mx9 -up1q1r2x2y2z1w2 -xr@../tools/exclusion.txt ../${FILE} system
 cd ..
