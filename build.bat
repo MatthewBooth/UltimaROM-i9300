a16499f36cf79f80c14790d2421ab32c619db79e
Kryten2k35 <kryten2k35@gmail.com>
2014-07-26 23:36:40 +0100

    EXPERIMENTAL: Compressed common items

--- build.bat
+++ build.bat
@@ -4,4 +4,6 @@ set /p VERSION=<META-INF\com\google\android\aroma\version.txt
 tools\7za.exe a -up0q0r2x2y2z1w2 -mx9 -xr@tools/exclusion.txt UltimaROM_%VERSION%_%CODENAME%.zip META-INF system ultima data
 cd overlay
 ..\tools\7za.exe u -mx9 -up1q1r2x2y2z1w2 -xr@..\tools\exclusion.txt ..\UltimaROM_%VERSION%_%CODENAME%.zip system
+cd ..\compressed
+..\tools\7za.exe u -mx9 -up1q1r2x2y2z1w2 -xr@..\tools\exclusion.txt ..\UltimaROM_%VERSION%_%CODENAME%.zip system
 cd ..
--- build.sh
+++ build.sh
@@ -8,4 +8,6 @@ FILE=UltimaROM_${VERSION}_${CODENAME}.zip
 tools/7za a -up0q0r2x2y2z1w2 -mx9 -xr@tools/exclusion.txt ${FILE} META-INF system ultima data
 cd overlay
 ../tools/7za u -mx9 -up1q1r2x2y2z1w2 -xr@../tools/exclusion.txt ../${FILE} system
+cd ../compressed
+../tools/7za u -mx9 -up1q1r2x2y2z1w2 -xr@../tools/exclusion.txt ../${FILE} system
 cd ..
