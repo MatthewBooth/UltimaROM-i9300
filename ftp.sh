7b00705b28ffa7485218bbd8433f665a099e1ce4
Kryten2k35 <kryten2k35@gmail.com>
2014-07-30 20:01:24 +0100

    BUILD: Removed copied file. Added Usage intructions

--- ftp.sh
+++ ftp.sh
@@ -5,6 +5,9 @@ CODENAME=$(cat ${AROMA}/codename.txt)
 VERSION=$(cat ${AROMA}/version.txt)
 FILE=UltimaROM_${VERSION}_${CODENAME}.zip
 
+## Usage
+# Run "sh ftp.sh ftp.example.com/folder/to/your/save/location username password"
+
 curl -T ${FILE} ftp://$1 --user $2:$3
 
 exit 0
